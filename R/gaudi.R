5e36388c3d12e8faa54ae579eb2525d88ff3b8c7
Frank Ockerman <frankockerman@MacBookPro.attlocal.net>
2025-07-31 10:27:17 -0400

    Fixed GAUDI bug where no splits argument. Remove CMSA support

--- R/gaudi.R
+++ R/gaudi.R
@@ -12,15 +12,16 @@
 #' @param ind_train An optional vector of indices corresponding to rows
 #' in the fbm matrix, used to subset samples for training. If provided,
 #' y_train must be the same length and sorted to these indices.
-##' @param verbose A boolean indicating verbosity
-##' @param minlam A numeric with the minimum lambda value for genlasso
-##' @param maxsteps_init An integer with the max steps for genlasso in
-## the initial model
-##' @param maxsteps_cv An integer with the max steps for genlasso in
-## cross-validation models
-##' @param gamma_vec A numeric vector with gamma values
-##' @param approx A boolean indicating whether
-## to use the approximate solution
+#' @param verbose A boolean indicating verbosity
+#' @param minlam A numeric with the minimum lambda value for genlasso
+#' @param maxsteps_init An integer with the max steps for genlasso in
+# the initial model
+#' @param maxsteps_cv An integer with the max steps for genlasso in
+# cross-validation models
+#' @param gamma_vec A numeric vector with gamma values
+#' @param approx A boolean indicating whether to use the approximate solution
+#' @param splits A list of k vectors indicating the indices
+#' (in the training set) for inner-CV folds
 #' @importFrom stats cor
 #' @importFrom genlasso fusedlasso getD1dSparse predict.genlasso
 #' @importFrom splitTools create_folds
@@ -32,12 +33,19 @@ gaudi <- function(fbm, fbm_info, y_train, ind_train = NULL,
                   gamma_vec, k = 10, variants = NULL,
                   verbose = FALSE, minlam = 0,
                   maxsteps_init = 2000, maxsteps_cv = 2000 * 10,
-                  approx = FALSE) {
+                  approx = FALSE, splits = NULL) {
   x_mat <- construct_gaudi(fbm, fbm_info, variants)
   if (is.null(ind_train)) {
     ind_train <- seq_len(nrow(x_mat))
   }
   x_mat <- x_mat[ind_train, ]
+
+  if (is.null(splits)) {
+    ind_sets <- sample(ind_train)
+    ind_sets <- as.numeric(cut(seq_along(ind_sets), k))[order(ind_sets)]
+    splits <- lapply(1:k, function(cv_fold) which(ind_sets != cv_fold))
+  }
+
   model <- cv_fused_lasso(
     x = x_mat, y = y_train, n_folds = k, verbose = verbose,
     minlam = minlam, maxsteps_init = maxsteps_init,
@@ -60,8 +68,7 @@ cv_fused_lasso <- function(x, y, n_folds,
     D = vector("list", length(gamma_vec)),
     best_lambda = rep(0, length(gamma_vec)),
     best_r2 = rep(0, length(gamma_vec)),
-    cv_r2_full = vector("list", length(gamma_vec)),
-    beta_cmsa = vector("list", length(gamma_vec))
+    cv_r2_full = vector("list", length(gamma_vec))
   )
 
   i <- 1
@@ -90,7 +97,6 @@ cv_fused_lasso <- function(x, y, n_folds,
     lambdas <- init_fit$lambda
     fold_scores <- vector("list", length = n_folds)
     n_ratio <- (n_folds - 1) / n_folds
-    fold_beta <- vector("list", length = n_folds)
 
     for (fold in 1:n_folds) {
       print(sprintf("Fitting fold %s", fold))
@@ -121,7 +127,6 @@ cv_fused_lasso <- function(x, y, n_folds,
     gamma_scores$cv_r2_full[[i]] <- cv_r2_full
     gamma_scores$best_lambda[i] <- best_lambda
     gamma_scores$best_r2[i] <- best_r2
-    gamma_scores$beta_cmsa[[i]] <- rowMeans(do.call(cbind, fold_beta))
     i <- i + 1
   }
 
@@ -134,8 +139,7 @@ cv_fused_lasso <- function(x, y, n_folds,
     "best_gamma" = gamma_scores$gamma[best_index],
     "best_lambda" = gamma_scores$best_lambda[best_index],
     "cv_r2" = gamma_scores$best_r2[best_index],
-    "full_cv_r2" = gamma_scores$cv_r2_full,
-    "beta_cmsa" = gamma_scores$beta_cmsa[[best_index]]
+    "full_cv_r2" = gamma_scores$cv_r2_full
   )
 
   return(out_list)
